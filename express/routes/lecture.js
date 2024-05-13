57070e846423795bf73225d1e708237fa3daa2ae
LauranceLi <lars108218@gmail.com>
2024-05-14 02:52:08 +0800

    增加路由规则

--- express/routes/lecture.js
+++ express/routes/lecture.js
@@ -3,8 +3,12 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+  res.status(200).json({message: `讲座`})
 })
+router.get('/:id', function (req, res, next) {
+  res.status(200).json({message: `讲座介绍`})
+})
+
 
 // router.get('/member', function (req, res, next) {
 //   res.render('index', { title: '123' })
