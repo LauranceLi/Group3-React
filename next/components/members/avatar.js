45c0c0109addc800b4e27c42dce8b90a5d1afdb6
LauranceLi <lars108218@gmail.com>
2024-05-19 02:05:55 +0800

    调整头像组件

--- next/components/members/avatar.js
+++ next/components/members/avatar.js
@@ -2,32 +2,23 @@ import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import styles from '/styles/login.module.css'
 
-function Avatar() {
+function Avatar({ width = 300, height = 300 }) {
   const [avatarPath, setAvatarPath] = useState('/images/cedarAvatar.jpg')
 
-  // useEffect(() => {
-  //   // 從資料庫中獲取網址的邏輯
-  //   const fetchImageUrl = async () => {
-  //     try {
-  //       const response = await fetch('YOUR_API_ENDPOINT');
-  //       const data = await response.json();
-  //       setImageUrl(data.imageUrl); // 假設資料中有一個叫做imageUrl的屬性存儲了圖片的網址
-  //     } catch (error) {
-  //       console.error('Error fetching image URL:', error);
-  //     }
-  //   };
-
-  //   fetchImageUrl();
-  // }, []);
+  // 将宽高保持相同以确保圆形
+  const size = Math.min(width, height);
 
   return (
-    <>
-      <div className={styles.avatarContainer}>
-        <div className={styles.avatar}>
-          <Image src={avatarPath} alt="Avatar" width={300} height={300} />
-        </div>
+    <div className={styles.avatarContainer}>
+      <div className={styles.avatar} style={{ width: size, height: size }}>
+        <Image
+          src={avatarPath}
+          alt="Avatar"
+          layout="fill" // 使用 fill 模式
+          objectFit="cover" // 确保图像填满容器并保持比例
+        />
       </div>
-    </>
+    </div>
   )
 }
 
