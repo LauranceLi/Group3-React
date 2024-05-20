ef4629f8f69f738b548ae3b8df64f6a1ff27a20c
Yun <159111847+XiaoYun3893z@users.noreply.github.com>
2024-05-20 10:25:20 +0800

    第一次更新

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -1,6 +1,8 @@
 import '/public/css/bootstrap.min.css'; // 确保路径正确
  // 确保路径正确
 import "/styles/globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 // import '/public/css/animate.css'; // 确保路径正确
 // import '/public/css/magnific-popup.css'; // 确保路径正确
 // import '/public/css/owl.carousel.css'; // 确保路径正确
