826c90e3808a54f6a4160f1051647af2d696f85b
Yun <159111847+XiaoYun3893z@users.noreply.github.com>
2024-05-21 12:16:28 +0800

    更新product-list.js

--- next/components/itinerary/product-list.js
+++ next/components/itinerary/product-list.js
@@ -23,43 +23,32 @@ export default function ProductList() {
               </div>
 
               <div className={styles.itineraryDetails}>
-              <div className={styles.itineraryTime}>
-                  {v.time}
-              </div>
-              <div>
-                <div className={styles.itineraryDescription}>{v.info}</div>
-              </div>
+                <div className={styles.itineraryTime}>{v.time}</div>
+                <div>
+                  <div className={styles.itineraryDescription}>{v.info}</div>
+                </div>
 
-              <div className={styles.itineraryDetailsIn}>
-                      <div className={styles.itineraryDetailsLeft}>
-                        <div className={styles.itineraryItem}>機位{v.seat}</div>
-                        <div className={styles.itineraryItem}>報名{v.number}</div>
-                        <div
-                          className={styles.itineraryItem}
-                          style={{ color: '#04abf2' }}
-                        >
-                          可售{v.sale}
-                        </div>
-                      </div>
-                      <div className={styles.itineraryDetailsRight}>
-                        <div className={styles.itineraryPriceSmall}>售價</div>
-                        <div className={styles.itineraryPriceAmount}>
-                          NT${v.price}
-                        </div>
-                        <div className={styles.itineraryPriceSmall}>起</div>
-                      </div>
+                <div className={styles.itineraryDetailsIn}>
+                  <div className={styles.itineraryDetailsLeft}>
+                    <div className={styles.itineraryItem}>機位{v.seat}</div>
+                    <div className={styles.itineraryItem}>報名{v.number}</div>
+                    <div
+                      className={styles.itineraryItem}
+                      style={{ color: '#04abf2' }}
+                    >
+                      可售{v.sale}
+                    </div>
+                  </div>
+                  <div className={styles.itineraryDetailsRight}>
+                    <div className={styles.itineraryPriceSmall}>售價</div>
+                    <div className={styles.itineraryPriceAmount}>
+                      NT${v.price}
+                    </div>
+                    <div className={styles.itineraryPriceSmall}>起</div>
+                  </div>
                 </div>
                 <div className={styles.flightDetails}>{v.airport}</div>
-
-
-
-
-
-
-
-
               </div>
-
             </div>
           )
         })}
