5975069d24d56b1b50404dd63791c656468c1eef
Yun <159111847+XiaoYun3893z@users.noreply.github.com>
2024-05-21 12:13:22 +0800

    更新行程商品頁面

--- next/pages/itinerary/itinerary-products2.js
+++ next/pages/itinerary/itinerary-products2.js
@@ -16,12 +16,12 @@ export default function itineraryProducts() {
           <div className="row">
             <div className="col-lg-3">
               <h3 className={styles.itineraryH3}>篩選項目</h3>
-                <div className={styles.itineraryProductsTitle}>價格區間</div>
-                <div className={styles.itineraryProductsItem1}>
-                  <div className={styles.SliderDiv}>
-                    <Slider1 />
-                  </div>
+              <div className={styles.itineraryProductsTitle}>價格區間</div>
+              <div className={styles.itineraryProductsItem1}>
+                <div className={styles.SliderDiv}>
+                  <Slider1 />
                 </div>
+              </div>
               <div className={styles.SliderDiv2}>
                 <div className={styles.itineraryProductsTitle}>天數</div>
                 <div className={styles.itineraryProductsItem1}>
@@ -66,58 +66,8 @@ export default function itineraryProducts() {
                 </div>
               </div>
 
-
               <div className={styles.itineraryContainer}>
-                <div className={styles.itineraryProductsItem3}>
-                  <div className={styles.itineraryProductsItem2}>
-                    <div>
-                      <Image
-                        src="/images/秘魯・印加帝國13日.jpg"
-                        alt="秘魯・印加帝國13日"
-                        width={336}
-                        height={250}
-                        priority={true}
-                      />
-                      <div className={styles.triangleContainer}></div>
-                      <div className={styles.itineraryProductsDays}>13天</div>
-                    </div>
-                  </div>
-                  <div className={styles.itineraryDetails}>
-                    <div>
-                      2024/07/09(二)
-                      <button className={styles.departureButton}>
-                        保證出發
-                      </button>
-                    </div>
-                    <div className={styles.itineraryDescription}>
-                      秘魯單國13日：世界七大奇蹟馬丘比丘、世界遺產納斯卡線、印加古城庫斯科、絕美打卡點彩虹山、高山淡水的的喀喀湖
-                    </div>
-                    <div className={styles.itineraryDetailsIn}>
-                      <div className={styles.itineraryDetailsLeft}>
-                        <div className={styles.itineraryItem}>機位31</div>
-                        <div className={styles.itineraryItem}>報名11</div>
-                        <div
-                          className={styles.itineraryItem}
-                          style={{ color: '#04abf2' }}
-                        >
-                          可售20
-                        </div>
-                      </div>
-                      <div className={styles.itineraryDetailsRight}>
-                        <div className={styles.itineraryPriceSmall}>售價</div>
-                        <div className={styles.itineraryPriceAmount}>
-                          NT$272,000
-                        </div>
-                        <div className={styles.itineraryPriceSmall}>起</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className={styles.flightDetails}>台北-桃園機場出發</div>
-                </div>
-                {/* 0521 */}
-
-              <ProductList />
-
+                <ProductList />
               </div>
             </div>
           </div>
