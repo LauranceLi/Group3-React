a9356e8fb0624f1247a4c00040c0dace47f1ef01
daniel1761590202 <daniel1761590944@gmail.com>
2024-05-22 12:19:27 +0800

    Revert "修改"
    This reverts commit 568bb1e11f50a85d31c4f6ec4818155af2f64536.

---
+++ next/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <h1>假裝有的首頁</h1>
+    </>
+  )
+}
