33c4b078a523b0a27dfddd1b07647d59fdb025f2
LauranceLi <lars108218@gmail.com>
2024-05-23 15:52:31 +0800

    增加頭像顯示功能

--- next/components/members/avatar.js
+++ next/components/members/avatar.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import styles from '@/styles/members/login.module.css'
 
 function Avatar({ width, height }) {
+  const [avatar, setAvatar] = useState('/images/flowers.jpg')
+
+  useEffect(() => {
+    const fetchAvatar = async () => {
+      try {
+        const res = await fetch('http://localhost:3005/api/members/get_info', {
+          credentials: 'include',
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        })
+
+        const data = await res.json()
+
+        if (data.status === 'success') {
+          setAvatar(data.data.avatar)
+        }
+      } catch (error) {
+        console.error('Failed to fetch avatar:', error)
+      }
+    }
+
+    fetchAvatar()
+    console.log(avatar)
+  }, [])
+  if (!avatar){
+setAvatar('/images/flowers.jpg')
+  }
+
   return (
     <div className={styles.avatarContainer}>
       <div className={styles.avatar} style={{ width: width, height: height }}>
+        {/* <h1>{avatar}</h1> */}
         <Image
-          src='/images/cedarAvatar.jpg'
+          src={avatar}
           alt="Avatar"
           layout="fill"
           objectFit="cover"
--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'avatars.githubusercontent.com'], // 添加允许的图像域名
+  },
 };
 
 export default nextConfig;
-
-
