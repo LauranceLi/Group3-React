9943b31e19b07ce513cb863017b98e804360c640
LauranceLi <lars108218@gmail.com>
2024-05-23 16:55:35 +0800

    引入preloader

--- next/pages/members/profile.js
+++ next/pages/members/profile.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ProfileInfo from '@/components/members/profile-info'
+import Preloader from '@/components/layout/preloader'
 import Navbar from '@/components/layout/navbar'
 import Footer from '@/components/layout/footer'
 
 export default function Register() {
   return (
     <>
+      <Preloader />
       <Navbar />
       <ProfileInfo />
-      <Footer/>
+      <Footer />
     </>
   )
 }
