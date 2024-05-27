b2a4e2cfc0476ca40313cc13ff107da50d20b100
Yun <159111847+XiaoYun3893z@users.noreply.github.com>
2024-05-28 03:54:38 +0800

    購物車第1頁完成

--- next/pages/itinerary-order/[pid].js
+++ next/pages/itinerary-order/[pid].js
@@ -1,82 +1,351 @@
 import { useState, useEffect } from 'react'
-
 import { useRouter } from 'next/router'
-import Link from 'next/link'
+import { loadProduct } from '@/services/itinerary-product'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { CiCircleMinus, CiCirclePlus } from 'react-icons/ci'
+import styles from '@/styles/itinerary.module.css'
+import Navbar from '@/components/layout/navbar'
+import Footer from '@/components/layout/footer'
 
-
-// 資料夾的中的`[pid].js`檔案代表這路由中，除了根路由與靜態路由之外的所有路由，例如 `/product/123` 就是這個檔案
-// 資料來源:
-// https://my-json-server.typicode.com/eyesofkids/json-fake-data/products/${pid}
-
-export default function Detail() {
-  // 第1步. 宣告能得到動態路由pid的路由器
-  // router.query(物件)，其中包含了pid屬性值
-  // router.isReady(布林)，如果是true代表頁面已完成水合作用，可以得到pid
-  const router = useRouter()
-
+export default function GroupCart() {
   const [product, setProduct] = useState({
-    id: 0,
-    picture: '',
-    stock: 0,
-    name: '',
+    travel_id: 0,
+    days: '',
+    time: '',
+    title: '',
+    seat: 0,
+    number: 0,
+    sale: 0,
     price: 0,
-    tags: '',
+    deposit_date: '',
+    final_payment_date: '',
   })
 
-  // 宣告一個指示是不是正在載入資料的狀態
-  // 因為一開始一定是要載入資料，所以預設值為true
-  const [isLoading, setIsLoading] = useState(true)
+  // 通過點擊icon去增和減，在input中產生數字
+  const [adultQuantity, setAdultQuantity] = useState(0)
+  const [childQuantity, setChildQuantity] = useState(0)
+  // 小計的金額計算
+  const totalAmount =
+    product.price * adultQuantity + product.price * childQuantity
 
+  // 減少成人數量
+  const decreaseAdultQuantity = () => {
+    if (adultQuantity > 0) {
+      setAdultQuantity(adultQuantity - 1)
+    }
+  }
+  // 增加成人數量
+  const increaseAdultQuantity = () => {
+    setAdultQuantity(adultQuantity + 1)
+  }
+  // 減少小孩佔床數量
+  const decreaseChildQuantity = () => {
+    if (childQuantity > 0) {
+      setChildQuantity(childQuantity - 1)
+    }
+  }
+  // 增加小孩佔床數量
+  const increaseChildQuantity = () => {
+    setChildQuantity(childQuantity + 1)
+  }
+
+  // 計算訂金的值（totalAmount的20%）
+  const depositAmount = totalAmount * 0.2
+
+  const router = useRouter()
+
+  // 使用 API 加載產品數據
   const getProduct = async (pid) => {
-    // const url = `https://my-json-server.typicode.com/eyesofkids/json-fake-data/products/${pid}`
-
-    // 要使用try...catch陳述式，讓與伺服器連線作REST更穩健
     try {
-      // const res = await fetch(url)
-      // const data = await res.json()
       const data = await loadProduct(pid)
-
-      console.log(data)
-
-      // 設定到狀態中 ===> 進入update階段，觸發重新渲染(re-render) ===> 顯示資料
-      // 確定資料是物件資料類型才設定到狀態中(最基本的保護)
       if (typeof data === 'object' && data !== null && !Array.isArray(data)) {
         setProduct(data)
-        // 關掉載入動畫，1.5s後
-        setTimeout(() => {
-          setIsLoading(false)
-        }, 1500)
       }
     } catch (e) {
       console.error(e)
     }
   }
 
-  // 樣式3: didMount+didUpdate
-  // 第2步: 在useEffect中監聽router.isReady為true時，才能得到網址上的pid，之後向伺服器要資料
   useEffect(() => {
-    console.log(router.query)
-
     if (router.isReady) {
       const { pid } = router.query
       getProduct(pid)
     }
-    // eslint-disable-next-line
   }, [router.isReady])
 
   return (
     <>
-      <h1>商品詳細頁</h1>
-      <hr />
-      <Link href="/itinerary-order/list">連至 列表頁</Link>
-      <br />
-
-        <>
-          <p>ID: {product.id}</p>
-          <p>名稱: {product.name}</p>
-          <p>價格: {product.price}</p>
-        </>
-      
+      {/* <Link href="/itinerary-order/list">連至 列表頁</Link> */}
+      <Navbar />
+      <main className={styles.GroupCart1}>
+        <br />
+        <Container>
+          <div className="mb-3">
+            <Row className={styles.GroupRow}>
+              <Col className={styles.activeCartTitle}>1. 訂購內容</Col>
+              <Col className={styles.first}>2. 填寫旅客資訊</Col>
+              <Col className={styles.first}>3. 完成訂單</Col>
+            </Row>
+            <div className={styles.second}>
+              <div className={styles.orderTitle}>
+                <h4 style={{ fontSize: '18px', fontWeight: 600 }}>
+                  訂購人資料
+                </h4>
+              </div>
+              <div className="row">
+                <div className="col m-2">
+                  <label htmlFor="name">
+                    <h6>訂購人姓名&nbsp;</h6>
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="form-control"
+                  />
+                </div>
+                <div className="col m-2">
+                  <label htmlFor="email">
+                    <h6>電子信箱&nbsp;</h6>
+                  </label>
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    className="form-control"
+                  />
+                </div>
+                <div className="col m-2">
+                  <label htmlFor="number">
+                    <h6>手機號碼&nbsp;</h6>
+                  </label>
+                  <input
+                    type="mobile"
+                    name="mobile"
+                    id="mobile"
+                    className="form-control"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={styles.second2}>
+              <div className={styles.orderTitle}>
+                <h4
+                  style={{ fontSize: '18px', fontWeight: 600 }}
+                  className="mb-3"
+                >
+                  訂購內容
+                </h4>
+                <h6
+                  className={styles.travelSaleItem}
+                  style={{ fontSize: '22px' }}
+                >
+                  {product.introduce}
+                </h6>
+                <span className={styles.groupCartSpan1}>
+                  出發日期:{product.time}
+                </span>
+              </div>
+              <div>
+                <div className={styles.travelInfo2}>
+                  <h6 className={styles.travelSaleItem}>請確認訂購內容</h6>
+                  <div className={styles.unitPrice}>單價</div>
+                  <div className={styles.unitPrice}>
+                    可售團位:
+                    <div className={styles.groupCartSeat}>{product.sale}</div>
+                  </div>
+                  <div className={styles.unitPrice}>小計</div>
+                </div>
+              </div>
+              <div className={styles.travelInfo2}>
+                <h6
+                  className={styles.travelSaleItem}
+                  style={{ fontSize: '16px' }}
+                >
+                  大人
+                </h6>
+                <div className={styles.unitPrice}>
+                  <span>NT$</span>
+                  {product.price}
+                </div>
+                <div className={styles.unitPrice}>
+                  <CiCircleMinus
+                    size={24}
+                    onClick={decreaseAdultQuantity}
+                    style={{ cursor: 'pointer' }}
+                  />
+                  <input
+                    type="number"
+                    name="adultQuantity"
+                    className={styles.quantity}
+                    value={adultQuantity}
+                    readOnly
+                  />
+                  <CiCirclePlus
+                    size={24}
+                    onClick={increaseAdultQuantity}
+                    style={{ cursor: 'pointer' }}
+                  />
+                </div>
+                <div className={styles.unitPrice}>
+                  <span>NT${product.price * adultQuantity}</span>
+                </div>
+              </div>
+              <div className={styles.travelInfo2}>
+                <h6
+                  className={styles.travelSaleItem}
+                  style={{ fontSize: '16px' }}
+                >
+                  小孩佔床
+                </h6>
+                <div className={styles.unitPrice}>
+                  <span>NT$</span>
+                  {product.price}
+                </div>
+                <div className={styles.unitPrice}>
+                  <CiCircleMinus
+                    size={24}
+                    onClick={decreaseChildQuantity}
+                    style={{ cursor: 'pointer' }}
+                  />
+                  <input
+                    type="number"
+                    name="childQuantity"
+                    className={styles.quantity}
+                    value={childQuantity}
+                    readOnly
+                  />
+                  <CiCirclePlus
+                    size={24}
+                    onClick={increaseChildQuantity}
+                    style={{ cursor: 'pointer' }}
+                  />
+                </div>
+                <div className={styles.unitPrice}>
+                  <span>NT${product.price * childQuantity}</span>
+                </div>
+              </div>
+              <span className={styles.unitPrice2}>
+                總計&nbsp;NT${totalAmount}
+              </span>
+            </div>
+            <div className="second mb-3">
+              <div className={styles.orderTitle2}>
+                <div className="row">
+                  <div className="col">
+                    <h4 style={{ fontSize: '22px' }}>
+                      訂單總金額 <span>NT${totalAmount}</span>
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="second mb-3">
+              <div className={styles.orderTitle2}>
+                <div className="row">
+                  <div className={styles.totalAmount1}>
+                    <h4 style={{ fontWeight: 600, fontSize: '20px' }}>訂金</h4>
+                    <div className={styles.groupCart2Money}>
+                      NT$&nbsp;
+                      <span style={{ color: 'tomato', fontWeight: 'bold' }}>
+                        {depositAmount}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.totalAmount}>
+                <h6 className={styles.deadline}>
+                  訂金付款期限 :&nbsp;
+                  <span className={styles.deadline}>
+                    {product.deposit_date}
+                  </span>
+                </h6>
+              </div>
+            </div>
+            <div className="second mb-3">
+              <div className={styles.orderTitle2}>
+                <div className="row">
+                  <div className={styles.totalAmount1}>
+                    <h4 style={{ fontWeight: 600, fontSize: '20px' }}>尾款</h4>
+                    <div className={styles.groupCart2Money}>
+                      NT$&nbsp;
+                      <span style={{ color: 'tomato', fontWeight: 'bold' }}>
+                        {totalAmount - depositAmount}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.totalAmount}>
+                <h6 className={styles.deadline}>
+                  尾款付款期限 :&nbsp;{product.final_payment_date}
+                </h6>
+              </div>
+            </div>
+            <div className={styles.second2}>
+              <div className="row p-2">
+                <div className="col">
+                  <ul>12</ul>
+                </div>
+              </div>
+            </div>
+            <div className={styles.second2}>
+              <div className="row p-2">
+                <div className="col">
+                  <h4 style={{ fontSize: '18px', fontWeight: 600 }}>
+                    觀光署發布之國外旅遊定型化契約
+                  </h4>
+                  <embed
+                    src="/合約書.pdf"
+                    type="application/pdf"
+                    className={styles.contractView}
+                    width="100%"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={styles.second2}>
+              <div className="row p-2">
+                <div className="col">
+                  <h4 style={{ fontSize: '18px', fontWeight: 600 }}>
+                    隱私權政策
+                  </h4>
+                  <p className={styles.viewTextScroll}></p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.agreement}>
+              <input type="checkbox" name="agreement" id="agreement" />
+              <label htmlFor="agreement" className={styles.agreement}>
+                已閱讀並同意「訂購須知」、「旅遊契約書」、及「隱私權政策」。
+              </label>
+              <div className={styles.agreementDiv}>
+                <div className="m-1">
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={() =>
+                      (window.location.href =
+                        'http://localhost:3000/itinerary-order/group-cart2')
+                    }
+                  >
+                    確認訂購
+                  </button>
+                </div>
+                <div className="m-1">
+                  <button type="reset" className="btn btn-secondary">
+                    取消訂購
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
