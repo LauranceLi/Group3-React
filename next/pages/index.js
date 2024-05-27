c9df6e89789ec345086b38990a29e85850d5b559
Yun <159111847+XiaoYun3893z@users.noreply.github.com>
2024-05-27 09:56:01 +0800

    主頁新增搜尋欄

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/home.module.css'
+import Search from '@/components/search/search'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <h1>假裝有的首頁</h1>
+      <Search />
     </>
   )
 }
