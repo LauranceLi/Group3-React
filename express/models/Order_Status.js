7e6568f7b58d951958e442b1fe1e1a645e73fb63
gewdszdftgr <helenhan46@gmail.com>
2024-05-28 17:39:14 +0800

    新增訂單查詢的router+component&page

--- express/models/Order_Status.js
+++ express/models/Order_Status.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { DataTypes } from 'sequelize'
 
 export default async function OrderStatus(sequelize) {
@@ -37,3 +38,44 @@ export default async function OrderStatus(sequelize) {
     }
   )
 }
+=======
+import { DataTypes } from 'sequelize'
+
+export default async function OrderStatus(sequelize) {
+  return sequelize.define(
+    'OrderStatus',
+    {
+      status_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      transaction_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      payment_status: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      shipping_status: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      order_status: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: 'order_status', //直接提供資料表名稱
+      timestamps: true, // 使用時間戳
+      paranoid: false, // 軟性刪除
+      underscored: true, // 所有自動建立欄位，使用snake_case命名
+      createdAt: 'created_at', // 建立的時間戳
+      updatedAt: 'updated_at',
+    }
+  )
+}
+>>>>>>> Stashed changes
---
+++ next/components/order/order_query.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react'
+
+function OrderQuery() {
+  const [orderData, setOrderData] = useState({ orders: [], orderDetails: [] })
+  // const memberId = localStorage.getItem('member_id'); // 从本地存储中获取 member_id
+  const memberId = '20150221008' // 替换成你要查詢的會員 ID
+
+  useEffect(() => {
+    fetch(`http://localhost:3005/api/order_query/20150221008`)
+      .then((response) => response.json())
+      .then((data) => {
+        setOrderData(data)
+      })
+      .catch((error) => console.error('Error fetching order data:', error))
+  }, [memberId])
+
+  // 組合訂單和訂單詳情
+  const combinedOrders = orderData.orders.map((order) => {
+    const details = orderData.orderDetails.filter(
+      (detail) => detail.transaction_id === order.transaction_id
+    )
+    return { ...order, details }
+  })
+
+  // 按照 created_at 日期從新到舊排序
+  const sortedOrders = combinedOrders.sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  )
+
+  return (
+    <div>
+      {/* <h2>Orders and Order Details for Member {memberId}</h2> */}
+      {sortedOrders.map((order) => (
+        <div key={order.transaction_id}>
+          <h4 className="bottom-line d-inline">交易編號: {order.transaction_id}</h4>
+          <div>
+            <div className="d-flex">
+              <div className="travel-saleitem">商品名稱</div>
+              <div className="me-4 unit-price text-center">數量</div>
+              <div className="me-4 unit-price text-center">單價</div>
+            </div>
+            {order.details.map((detail) => (
+              <div key={detail.detail_id} className="d-flex mb-3">
+                <p className="travel-saleitem"> {detail.product_name}</p>
+                <p className="me-4 unit-price text-center">
+                  {detail.quantity}{' '}
+                </p>
+                <p className="me-4 unit-price text-center">
+                  {detail.unit_price}
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="d-flex">
+            <p className="me-4">總金額: {order.total_amount}</p>
+            <p>折扣後金額: {order.net_total}</p>
+          </div>
+          <p>
+            送貨地址:
+            {order.country}
+            {order.township}
+            {order.shipping_address}
+            {order.store_name}
+          </p>
+          <p>下單時間: {new Date(order.created_at).toLocaleString()}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default OrderQuery
