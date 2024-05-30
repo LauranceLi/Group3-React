ce7bcbe0c3b4374b64c9a47825a34e951d5c4915
LauranceLi <lars108218@gmail.com>
2024-05-31 02:31:10 +0800

    講座頁（剩下講者資訊）

--- next/pages/index.js
+++ next/pages/index.js
@@ -6,6 +6,7 @@ import Slider from '@/components/slider';
 import Preloader from '@/components/layout/preloader'
 import Navbar from '@/components/layout/navbar'
 import Footer from '@/components/layout/footer'
+import Drawer from '@/components/layout/drawer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,9 +14,11 @@ export default function Home() {
   return (
     <>
     <Preloader />
+
     <Navbar />
     <Slider />
     <Footer />
+    
     </>
   )
 }
