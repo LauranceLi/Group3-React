637bbe4799140bead01d7a147fd8746b5ae8c25e
LauranceLi <lars108218@gmail.com>
2024-05-31 11:19:56 +0800

    調整樣式

--- next/components/point/point.js
+++ next/components/point/point.js
@@ -17,34 +17,35 @@ export default function Point() {
         <span>可用點數: {points}點</span>
       </div>
       <div className="d-flex col-6 m-2">
-      <div className="d-flex col-6 m-2">
-        <button
-          onClick={handleDecrease}
-          disabled={discountAmount <= 0}
-          className="no-border"
-        >
-          <h5 className="m-0">
-            <SquareMinus />
-          </h5>
-        </button>
-        <input
-          type="number"
-          value={discountAmount}
-          onChange={handleDiscountChange}
-          min="0"
-          step="10"
-          disabled={true}
-          className="form-control text-center"
-        />
-        <button
-          onClick={handleIncrease}
-          disabled={discountAmount >= Math.min(points / 300, 100)}
-          className="no-border"
-        >
-          <h5 className="m-0">
-            <SquarePlus />
-          </h5>
-        </button>
+        <div className="d-flex col-6 m-2">
+          <button
+            onClick={handleDecrease}
+            disabled={discountAmount <= 0}
+            className="no-border"
+          >
+            <h5 className="m-0">
+              <SquareMinus />
+            </h5>
+          </button>
+          <input
+            type="number"
+            value={discountAmount}
+            onChange={handleDiscountChange}
+            min="0"
+            step="10"
+            disabled={true}
+            className="form-control text-center"
+          />
+          <button
+            onClick={handleIncrease}
+            disabled={discountAmount >= Math.min(points / 300, 100)}
+            className="no-border"
+          >
+            <h5 className="m-0">
+              <SquarePlus />
+            </h5>
+          </button>
+        </div>
       </div>
       <div className="m-2">
         <span>折扣金額: ${discountAmount}</span>
--- next/pages/cart/order_query.js
+++ next/pages/cart/order_query.js
@@ -2,8 +2,6 @@ import React from 'react'
 import OrderQuery from '@/components/order/order_query'
 import Navbar from '@/components/layout/navbar'
 import Footer from '@/components/layout/footer'
-import Navbar from '@/components/layout/navbar'
-import Footer from '@/components/layout/footer'
 export default function Test() {
   return (
     <>
